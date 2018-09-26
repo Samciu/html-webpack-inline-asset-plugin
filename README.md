c9bde882a9798fa5be071854676a4169e0605a58
ou_shen_zhao <oushenzhao@zuzuche.com>
2018-09-27 00:13:55 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # html-webpack-inline-asset-plugin
+
+> Inline all the css and js assets in the html where the `<script>`, `<link>` tags that contain the `inline` attribute.
+
+## Installation
+
+
+```shell
+npm install --save html-webpack-inline-asset-plugin
+```
+
+## Basic Usage
+Require the plugin in your webpack config:
+
+```javascript
+var HtmlWebpackInlineAssetPlugin = require('html-webpack-inline-asset-plugin');
+```
+
+Add the plugin to your webpack config as follows:
+
+```javascript
+plugins: [
+  new HtmlWebpackPlugin({inject: false}),
+  new HtmlWebpackInlineAssetPlugin()
+]  
+```
+
+Add the tag to your html template as follows:
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" inline>
+  </head>
+  <body>
+    <script inline></script>
+  </body>
+</html>
+```
+
+
+
+# License
+
+This project is licensed under [MIT](https://github.com/jantimon/html-webpack-plugin/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,86 @@
+const ReStyleTag = /<link[^>].*inline.*>/g;
+const ReScriptTag = /<script.*inline.*>/g;
+
+function createStyleTag(content) {
+    return `<style>${content}</style>`;
+}
+
+function createScriptTag(content) {
+    return `<script>${content}</script>`;
+}
+
+function HtmlWebpackInlinePlugin(options) {
+    this.options = options;
+}
+
+HtmlWebpackInlinePlugin.prototype.getRelativeDirPaths = function (compilation, results) {
+    const assetsKeys = Object.keys(compilation.assets);
+    let paths = []
+    results.forEach(result => {
+        let path = assetsKeys.filter(assetsKey => {
+            return result.includes(assetsKey)
+        })[0];
+        path && paths.push(path);
+    })
+    return paths;
+}
+
+HtmlWebpackInlinePlugin.prototype.apply = function (compiler) {
+    let self = this;
+    compiler.plugin('compilation', function (compilation, options) {
+        compilation.plugin('html-webpack-plugin-after-html-processing', function (htmlPluginData, callback) {
+            let result = self.processAssetsMapHtml(compilation, htmlPluginData);
+            callback(null, result);
+        });
+    });
+};
+
+HtmlWebpackInlinePlugin.prototype.processAssetsMapHtml = function (compilation, pluginData) {
+    let self = this;
+    let html = pluginData.html;
+
+    if (compilation.options.devServer === undefined) {
+        // 内联形式
+        html = html.replace(ReStyleTag, (match, p1, offset, string) => {
+            let result = match;
+
+            let relativeDirPaths = self.getRelativeDirPaths(compilation, pluginData.assets.css);
+
+            if (!relativeDirPaths.length) {
+                return result;
+            }
+
+            let source = '';
+            relativeDirPaths.map(relativeDirPath => {
+                source += compilation.assets[relativeDirPath].source();
+            });
+
+            result = createStyleTag(source);
+
+            return result;
+        });
+
+        html = html.replace(ReScriptTag, (match, p1, offset, string) => {
+            let result = match;
+
+            let relativeDirPaths = self.getRelativeDirPaths(compilation, pluginData.assets.js);
+
+            if (!relativeDirPaths.length) {
+                return result;
+            }
+
+            let source = '';
+            relativeDirPaths.map(relativeDirPath => {
+                source += compilation.assets[relativeDirPath].source();
+            });
+
+            result = createScriptTag(source);
+
+            return result;
+        });
+    }
+
+    return {html}
+};
+
+module.exports = HtmlWebpackInlinePlugin;
